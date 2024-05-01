d756ca594351ccc5faf21a52a8ad989ebfab119b
Cmarsman <chris_marsden@yahoo.com>
2024-05-01 10:51:42 -0500

    created exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("I hate Math!");
+                    break;
+                case "science":
+                    Console.WriteLine("I love science!");
+                    break;
+                case "pe":
+                    Console.WriteLine("PE is the best!");
+                    break;
+                case "history":
+                    Console.WriteLine("Histopry is fun.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is boring");
+                    break;
+                default
+                    : Console.WriteLine("I dont know that subject.");
+                    break;
+            }
         }
     }
 }
